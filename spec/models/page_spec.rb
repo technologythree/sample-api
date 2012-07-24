10d0a52be9d2d54fcce8d04431c0a375e03e7703
Pragnesh Vaghela <admin@technologythree.com>
2012-07-24 02:27:46 -0500

    updated test wordings

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe Page do
-  it "has a valid factory" do
+  it "should be a valid factory" do
     FactoryGirl.create(:page).should be_valid
   end
   
-  it "is invalid without a title" do
+  it "should not save page without a title" do
     FactoryGirl.build(:page, title: nil).should_not be_valid
   end
   
-  it "is invalid without content" do
+  it "should not save page without content" do
     FactoryGirl.build(:page, content: nil).should_not be_valid
   end
   
-  it "is valid without published on date" do
+  it "should save page without published on date" do
     FactoryGirl.build(:page, published_on: nil).should be_valid
   end
 end
