b564d7ad59f69e595345a8f8003c430eee4218af
Pragnesh Vaghela <admin@technologythree.com>
2012-07-26 01:10:36 -0500

    refactored tests and added comments

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -3,44 +3,58 @@ class Api::PagesController < ApplicationController
   
   before_filter :find_a_page, :only => [:show, :edit, :update, :destroy, :publish, :total_words]
 
+  # curl http://localhost:3000/api/pages.json
+  # curl http://localhost:3000/api/pages.xml
   def index
      @pages = Page.all
      respond_with(@pages)
    end
 
+  # curl http://localhost:3000/api/pages/:id.json
+  # curl http://localhost:3000/api/pages/:id.xml
    def show
      respond_with(@page)
    end
    
+  # curl http://localhost:3000/api/pages/new.json
+  # curl http://localhost:3000/api/pages/new.xml
    def new
      @page = Page.new
      respond_with(@page)
    end
 
+   # curl -d "page[title]=Test" -d "page[content]=JSON Page" -X POST http://localhost:3000/api/pages.json
+   # curl -d "page[title]=Test" -d "page[content]=XML Page" -X POST http://localhost:3000/api/pages.xml
    def create
      @page = Page.new(params[:page])
      if @page.save
-       respond_with(@page)
+       respond_with(@page, location: api_pages_url)
      else
-       respond_with(@page.errors)
+       respond_with(@page)
      end
    end
 
+   # curl http://localhost:3000/api/pages/:id/edit.json
+   # curl http://localhost:3000/api/pages/:id/edit.xml
    def edit
      respond_with(@page)
    end
 
+   # curl -d "page[title]=Test" -d "page[content]=JSON Page" -X PUT http://localhost:3000/api/pages/:id.json
+   # curl -d "page[title]=Test" -d "page[content]=XML Page" -X PUT http://localhost:3000/api/pages/:id.xml
    def update
      if @page.update_attributes(params[:page])
        respond_with(@page)
      else
-       respond_with(@page.errors)
+       respond_with(@page)
      end
    end
 
+   # curl -X DELETE http://localhost:3000/api/pages/:id.json
+   # curl -X DELETE http://localhost:3000/api/pages/:id.xml
    def destroy
      @page.delete
-     @message = {message: 'The Page was successfully deleted'}
+     @message = {message: 'Page deleted'}
      respond_with(@message)
    end
 
@@ -61,19 +75,20 @@ class Api::PagesController < ApplicationController
    end
    
    # Will mark a page as updated
-   # curl -X POST http://localhost:3000/api/pages/1/publish.json
-   # curl -X POST http://localhost:3000/api/pages/1/publish.xml
+   # curl -X POST http://localhost:3000/api/pages/:id/publish.json
+   # curl -X POST http://localhost:3000/api/pages/:id/publish.xml
    def publish
      @page.publish
      respond_with(@page, location: published_api_pages_url)
    end
    
    # Will return a total word count of title + content for a page
-   # curl http://localhost:3000/api/pages/1/total_words.json
-   # curl http://localhost:3000/api/pages/1/total_words.xml
+   # curl http://localhost:3000/api/pages/:id/total_words.json
+   # curl http://localhost:3000/api/pages/:id/total_words.xml
    def total_words
      respond_with(total_words: @page.total_words)
    end
+   
 
 private
 
--- spec/controllers/api/pages_controller_spec.rb
+++ spec/controllers/api/pages_controller_spec.rb
@@ -3,36 +3,37 @@ require 'spec_helper'
 describe Api::PagesController do
   
   describe 'GET #index' do
-    it "should populate an array of pages" do
+    it "should get json format list of pages" do
       page = FactoryGirl.create(:page)
-      get :index
-      assigns(:pages).should == [page]
+      get :index, format: "json"
+      response.should be_ok
+      response.content_type.should eq "application/json"
+      b = JSON.parse(response.body)[0]
+      b['title'].should eq (page['title'])
     end
     
-    it "renders the index json view" do
-      get :index
-      response.should render_template :json => "index"      
-    end
-    
-    it "should render xml view" do
-      get :index
-      response.should render_template :xml => "index"      
+    it "should get xml format list of pages" do
+      page = FactoryGirl.create(:page)
+      get :index, format: "xml"
+      response.should be_ok
+      response.content_type.should eq "application/xml"
+      b = Nokogiri::XML(response.body)
+      b.css("title").text.should eq (FactoryGirl.build(:page).title) 
     end
   end
   
   describe 'GET #new' do
-    it "should get a new Page " do
-      get :new
-      assigns(:page).should be_a_new(Page)
-    end
-    
-    it "should get a new json template" do
-      get :new
+    it "should get a new json Page " do
+      get :new, format: "json"
+      response.should be_ok
+      response.content_type.should eq "application/json"
       response.should render_template :json => "new"
     end
     
-    it "should get a new xml template" do
-      get :new
+    it "should get a new xml page" do
+      get :new, format: "xml"
+      response.should be_ok
+      response.content_type.should eq "application/xml"
       response.should render_template :xml => "new"
     end
   end
@@ -40,28 +41,48 @@ describe Api::PagesController do
   describe 'GET #edit' do
     it "should get a json edit page" do
       page = FactoryGirl.create(:page)
-      get :edit, id: page
+      get :edit, id: page, format: "json"
+      b = JSON.parse(response.body)
+      b['title'].should eq (page['title'])
+      response.should be_ok
+      response.content_type.should eq "application/json"
       response.should render_template :json => "edit"
     end
     
     it "should get a xml edit page" do
       page = FactoryGirl.create(:page)
-      get :edit, id: page
+      get :edit, id: page, format: "xml"
+      response.should be_ok
+      response.content_type.should eq "application/xml"
+      b = Nokogiri::XML(response.body)
+      b.css("title").text.should eq (page.title) 
       response.should render_template :xml => "edit"
     end
   end
 
-  describe "POST #create" do
-    it "should save a new page" do
-      expect {
-        post :create, page: FactoryGirl.attributes_for(:page)
-      }.to change(Page, :count).by(1)
+  describe "POST #create" do    
+    it "should save a new page json format" do
+      post :create, page: FactoryGirl.attributes_for(:page), format: "json"
+      response.content_type.should eq "application/json"
+      Page.count.should eq (1)
     end
     
-    it "should not save an invalid page" do
-      expect {
-        post :create, page: FactoryGirl.attributes_for(:invalid_page)
-      }.to_not change(Page, :count)
+    it "should not save an invalid page json format" do
+      post :create, page: FactoryGirl.attributes_for(:invalid_page), format: "json"
+      response.content_type.should eq "application/json"
+      Page.count.should eq (0)
+    end
+    
+    it "should save a new page xml format" do
+      post :create, page: FactoryGirl.attributes_for(:page), format: "xml"
+      response.content_type.should eq "application/xml"
+      Page.count.should eq (1)
+    end
+    
+    it "should not save an invalid page xml format" do
+      post :create, page: FactoryGirl.attributes_for(:invalid_page), format: "xml"
+      response.content_type.should eq "application/xml"
+      Page.count.should eq (0)
     end
   end
 
@@ -70,15 +91,20 @@ describe Api::PagesController do
      @page = FactoryGirl.create(:page)
     end
     
-    it "should populate requested page" do
-      put :update, id: @page, page: FactoryGirl.attributes_for(:page)
-      assigns(:page).should eq(@page)
+    it "should populate requested page json format" do
+      put :update, id: @page, page: FactoryGirl.attributes_for(:page), format: "json"
+      response.content_type.should eq "application/json"
+      @page.reload
+      @page.title.should eq (FactoryGirl.build(:page).title)
+      response.body.should be_blank
     end
     
-    it "should update page attributes" do
-      put :update, id: @page, page: FactoryGirl.attributes_for(:page, title: "This is a test title")
+    it "should populate requested page xml format" do
+      put :update, id: @page, page: FactoryGirl.attributes_for(:page), format: "xml"
+      response.content_type.should eq "application/xml"
       @page.reload
-      @page.title.should eq("This is a test title")
+      @page.title.should eq (FactoryGirl.build(:page).title)
+      response.body.should be_blank
     end
   end
 
@@ -87,10 +113,14 @@ describe Api::PagesController do
      @page = FactoryGirl.create(:page)
     end
     
-    it "should delete a page"do
-      expect{
-       delete :destroy, id: @page
-      }.to change(Page,:count).by(-1)
+    it "should delete a page json format"do
+      delete :destroy, id: @page, format: "json"
+      Page.count.should eq (0)
+    end
+    
+    it "should delete a page xml format"do
+      delete :destroy, id: @page, format: "xml"
+      Page.count.should eq (0)
     end
   end
   
@@ -100,10 +130,8 @@ describe Api::PagesController do
       get :published, format: "json"
       response.should be_ok
       response.content_type.should eq "application/json"
-      b = JSON.parse(response.body)
-      b.should be_a_kind_of(Array)
-      b.size.should eq 1
-      b[0]['title'].should eq (FactoryGirl.create(:page).title)
+      b = JSON.parse(response.body)[0]
+      b['title'].should eq (FactoryGirl.create(:page).title)
     end
     
     it "should get xml list of Published pages" do
@@ -115,18 +143,16 @@ describe Api::PagesController do
       b.css("title").text.should eq (FactoryGirl.create(:page).title)
     end
     
-    it "should return Unpublished page count" do
+    it "should return Unpublished page count for nil published on date json format" do
        post :create, page: FactoryGirl.attributes_for(:page, published_on: nil)
        get :unpublished, format: "json"
        response.should be_ok
        response.content_type.should eq "application/json"
-       b = JSON.parse(response.body)
-       b.should be_a_kind_of(Array)
-       b.size.should eq 1
-       b[0]['title'].should eq (FactoryGirl.create(:page).title)
+       b = JSON.parse(response.body)[0]
+       b['title'].should eq (FactoryGirl.create(:page).title)
     end
     
-    it "should return Unpublished page count for future publish date" do
+    it "should return Unpublished page count for future published on date xml format" do
       post :create, page: FactoryGirl.attributes_for(:page, published_on: DateTime.tomorrow)
       get :unpublished, format: "xml"
       b = Nokogiri::XML(response.body)
@@ -135,14 +161,14 @@ describe Api::PagesController do
       b.css("title").text.should eq (FactoryGirl.create(:page).title)
     end
 
-    it "should mark a page as published" do
+    it "should mark a page as published json format" do
       page = FactoryGirl.create(:page, published_on: nil)
       post :publish, id: page, format: "json"
       b = JSON.parse(response.body)
       b['published_on'].should_not be_nil
     end
     
-    it "should mark a page as published" do
+    it "should mark a page as published xml format" do
       page = FactoryGirl.create(:page, published_on: nil)
       post :publish, id: page, format: "xml"
       b = Nokogiri::XML(response.body)
@@ -155,7 +181,7 @@ describe Api::PagesController do
       response.should be_ok
       response.content_type.should eq "application/xml"
       b = Nokogiri::XML(response.body)
-      b.css("total-words").text.should eql("2")
+      b.css("total-words").text.should eq ("2")
     end
     
     it "should return json total page title and content count" do
